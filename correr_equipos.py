507dd6363cdb785d3b5d5f296eb9db32b509b3a9
Cristina Pérez Ramos <99310829+cperezramos@users.noreply.github.com>
2026-06-19 15:46:59 -0600

    Delete correr_equipos.py

--- correr_equipos.py
+++
@@ -1,8 +0,0 @@
-from src.team_id import identificar_equipos
-
-df, equipos = identificar_equipos(
-    ruta_video=r"dataset/camara_superior/recorte_2min.mov",
-    csv_robots="data/trayectorias_limpias.csv",
-    salida_csv="data/trayectorias_equipos.csv",
-)
-print("\nResumen:", df.groupby("equipo")["tracker_id"].nunique(), "robots por equipo")
